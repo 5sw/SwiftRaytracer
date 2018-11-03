38018b605dd8754c18423b4124619823a61221cc
Sven Weidauer <sven@5sw.de>
2018-11-03 10:46:41 +0100

    Initial Commit

---
+++ Raytracer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Raytracer
+//
+//  Created by Sven Weidauer on 03.11.18.
+//  Copyright © 2018 Sven Weidauer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
