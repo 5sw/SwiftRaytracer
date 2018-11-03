2c7c67ffe01c0ed7046540a23b3cd0ef34db0a12
Sven Weidauer <sven@5sw.de>
2018-11-03 11:41:51 +0100

    Support per-object material

--- Raytracer/main.swift
+++ Raytracer/main.swift
@@ -25,28 +25,48 @@ struct Intersection {
     let point: Point
     let normal: Vector
     let distance: Double
+    let object: Object
 
-    init(ray: Ray, distance: Double, normal: Vector) {
+    init(ray: Ray, distance: Double, normal: Vector, object: Object) {
         self.point = ray.at(distance)
         self.normal = normal
         self.distance = distance
+        self.object = object
     }
 
-    init(point: Point, distance: Double, normal: Vector) {
+    init(point: Point, distance: Double, normal: Vector, object: Object) {
         self.point = point
         self.distance = distance
         self.normal = normal
+        self.object = object
     }
 }
 
-protocol Object {
-    func intersect(ray: Ray) -> Intersection?
+typealias Color = vector_double3
+
+struct Material {
+    let ambient: Color
+    let diffuse: Color
+    let specular: Color
+    let shininess: Double
+
+    let reflecting: Bool
+    let reflectingPower: Color
 }
 
-struct Sphere: Object {
+protocol Object: class {
+    func intersect(ray: Ray) -> Intersection?
+    var material: Material { get }
+}
+
+class Sphere: Object {
     let center: Point
     let radius: Double
 
+    init(center: Point, radius: Double) {
+        self.center = center
+        self.radius = radius
+    }
 
     func intersect(ray: Ray) -> Intersection? {
         let m = ray.origin - center
@@ -68,13 +88,29 @@ struct Sphere: Object {
         let p = ray.at(t)
         let normal = normalize(p - center)
 
-        return Intersection(point: p, distance: t, normal: normal)
+        return Intersection(point: p, distance: t, normal: normal, object: self)
+    }
+
+    var material: Material {
+        return Material(
+            ambient: [1, 0, 0],
+            diffuse: [1, 0, 0],
+            specular: [1, 0, 1],
+            shininess: 40,
+            reflecting: false,
+            reflectingPower: [0, 0, 0]
+        )
     }
 }
 
-struct Plane: Object {
+class Plane: Object {
     let point: Point
     let normal: Vector
+
+    init(point: Point, normal: Vector) {
+        self.point = point
+        self.normal = normal
+    }
 
     func intersect(ray: Ray) -> Intersection? {
         let denom = dot(normal, ray.direction)
@@ -83,7 +119,18 @@ struct Plane: Object {
         let t = dot(point - ray.origin, normal) / denom
         guard t > 0 else { return nil }
 
-        return Intersection(ray: ray, distance: t, normal: normal)
+        return Intersection(ray: ray, distance: t, normal: normal, object: self)
+    }
+
+    var material: Material {
+        return Material(
+            ambient: [1, 1, 1],
+            diffuse: [1, 1, 1],
+            specular: [1, 1, 1],
+            shininess: 100,
+            reflecting: true,
+            reflectingPower: [1, 1, 1] * 0.5
+        )
     }
 }
 
@@ -132,7 +179,6 @@ let bytesPerLine = imageWidth * 4
 let pixels = UnsafeMutablePointer<UInt32>.allocate(capacity: imageWidth * imageHeight)
 pixels.initialize(repeating: 0, count: imageHeight * imageWidth)
 
-typealias Color = vector_double3
 
 extension Color {
     var pixelValue: UInt32 {
@@ -148,21 +194,7 @@ let ambient: Color = [1, 1, 1] * 0.1
 let diffuse: Color = [1, 1, 1] * 0.4
 let specular: Color = [1, 1, 1] * 0.8
 
-struct Material {
-    let ambient: Color
-    let diffuse: Color
-    let specular: Color
-    let shininess: Double
-}
-
-let material = Material(
-    ambient: [1, 0, 0],
-    diffuse: [1, 0, 0],
-    specular: [1, 0, 1],
-    shininess: 40
-)
-
-func shade(intersection: Intersection, material: Material) -> Color {
+func shade(intersection: Intersection, material: Material, depth: Int) -> Color {
     let ambientColor = material.ambient * ambient
 
     let lightRay = Ray(from: intersection.point, to: light)
@@ -179,6 +211,21 @@ func shade(intersection: Intersection, material: Material) -> Color {
         + material.specular * pow(max(0, dot(r, v)), material.shininess) * specular
 }
 
+let background: Color = [0, 0, 0]
+let maxDepth = 10
+
+var firedRays = 0
+
+func trace(ray: Ray, depth: Int = 0) -> Color {
+    guard depth < maxDepth else { return background }
+
+    firedRays += 1
+    guard let intersection = intersect(ray: ray) else { return background }
+
+    return shade(intersection: intersection, material: intersection.object.material, depth: depth)
+}
+
+
 DispatchQueue.concurrentPerform(iterations: imageHeight) { y in
     let yScreen = height * (0.5 - Double(y) / Double(imageHeight))
 
@@ -190,10 +237,7 @@ DispatchQueue.concurrentPerform(iterations: imageHeight) { y in
         let screenPoint = camera + distance * forward + xScreen * right + yScreen * up
 
         let ray = Ray(from: camera, to: screenPoint)
-
-        if let intersection = intersect(ray: ray) {
-            pixels[offset] = shade(intersection: intersection, material: material).pixelValue
-        }
+        pixels[offset] = trace(ray: ray).pixelValue
 
         offset += 1
     }
